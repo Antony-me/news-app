39fb435b81601860a6ad321d474c1905a3b8631f
Antony Simiyu <antonymunyasia993@gmail.com>
2020-10-16 13:32:30 +0300

    First commit

---
+++ app/models/news.py
@@ -0,0 +1,9 @@
+class News:
+    '''
+    News class to define Movie Objects
+    '''
+
+    def __init__(self,image,description,timeCreated):
+        self.image =image
+        self.description = description
+        self.timeCreated = timeCreated
