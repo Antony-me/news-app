dd2956b7fc45c9bc64557a3586fb9b81bf11dc0c
Antony Simiyu <antonymunyasia993@gmail.com>
2020-10-19 06:24:47 +0300

    fixed the request function

--- app/request.py
+++ app/request.py
@@ -1,4 +1,19 @@
 from app import app
+import urllib.request,json
+from .models import news
+from newsapi import NewsApiClient
 
-# Getting api key
-api_key = app.config['NEWS_API_KEY']
+News = news.News
+
+def get_news(everything):
+
+    newsapi =NewsApiClient(api_key='7d93df81198e4704b7a7a1a88d2e652b')
+
+    data = newsapi.get_everything(q= 'jupyter lab', language= 'en', page_size=20)
+
+
+    news_articles = data['articles']
+
+
+    return news_articles
+
