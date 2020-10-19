3eee227bfb5ed6e606cc27350b366e49d7051210
Antony Simiyu <antonymunyasia993@gmail.com>
2020-10-19 16:57:26 +0300

    Added search form

--- app/request.py
+++ app/request.py
@@ -1,3 +1,4 @@
+import newsapi
 from app import app
 import urllib.request,json
 from .models import news
@@ -29,3 +30,25 @@ def get_aljazeera_news(headlines):
     headlines = newsapi.get_top_headlines(sources ="al-jazeera-english")
 
     return headlines
+
+# def get_search(keyword):
+
+#     newsapi =NewsApiClient(api_key='7d93df81198e4704b7a7a1a88d2e652b')
+
+#     headlines = newsapi.get("https://newsapi.org/v2/everything?q=bitcoin&apiKey=7d93df81198e4704b7a7a1a88d2e652b")
+
+#     return headlines
+
+
+def search_news(keyword):
+    search_news_url = 'https://newsapi.org/v2/everything?q={}&apiKey=7d93df81198e4704b7a7a1a88d2e652b'.format(keyword)
+    with urllib.request.urlopen(search_news_url) as url:
+        search_news_data = url.read()
+        search_news_response = json.loads(search_news_data)
+
+        search_results = search_news_response
+
+
+    return search_results
+
+
--- app/views.py
+++ app/views.py
@@ -3,7 +3,8 @@ from flask import render_template,request,redirect,url_for
 from app import app
 
 
-from .request import get_aljazeera_news, get_bbc_news, get_news
+
+from .request import get_aljazeera_news, get_bbc_news, get_news, search_news
 
 @app.route('/')
 def index():
@@ -15,7 +16,14 @@ def index():
 
     headline = news['articles']
 
-    return render_template('index.html', headline = headline)
+    search_movie = request.args.get('news_query')
+
+    if search_news:
+
+        return redirect(url_for('search',news_name=search_news))
+    else:
+
+        return render_template('index.html', headline = headline)
 
 @app.route('/bbc')
 def bbc():
